c44899093b9e29db4ba94564b8086a11560eff0b
chevallier <fcr@abes.fr>
2021-02-09 19:46:35 +0100

    start hello

--- src/main/java/fr/corp/hello/controller/HelloController.java
+++ src/main/java/fr/corp/hello/controller/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
      @RequestMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
     public String test1()  {
 
-         String res = "hello power";
+         String res = "hello power docker";
         log.info("Step 0 start hello");
 
 
