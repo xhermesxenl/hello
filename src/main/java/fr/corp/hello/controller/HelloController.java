852bfc136b0f52583070cc913abceb3cb9abffc1
chevallier <fcr@abes.fr>
2021-02-09 18:41:12 +0100

    start hello

---
+++ src/main/java/fr/corp/hello/controller/HelloController.java
@@ -0,0 +1,28 @@
+package fr.corp.hello.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+@RestController
+@Slf4j
+public class HelloController {
+
+     @RequestMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String test1()  {
+
+         String res = "hello";
+        log.info("Step 0 start hello");
+
+
+
+
+
+        log.info("Step 1 end hello");
+        return res;
+    }
+
+
+}
